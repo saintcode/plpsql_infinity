205f673b39414be106812616be9a948c03d9c1e1
User <User>
2016-02-25 00:25:57 +0200

    create table script

---
+++ create_remove_table.sql
@@ -0,0 +1,25 @@
+﻿
+DROP TABLE IF EXISTS public."AA_remove";
+DROP SEQUENCE IF EXISTS public."AA_remove_id_seq";
+
+--CREATE SEQUENCE public."AA_remove_id_seq"
+--	INCREMENT 1
+--	MINVALUE 1
+--	MAXVALUE 9223372036854775807
+--	START 1
+--	CACHE 1;
+
+CREATE TABLE public."AA_remove"
+(
+	id bigserial NOT NULL,
+	name text,
+	phone_1 text,
+	phone_2 text,
+	email text--,
+	--CONSTRAINT "AA_remove_id_seq" PRIMARY KEY ("id")
+)
+WITH (
+	OIDS=FALSE
+);
+ALTER TABLE public."AA_remove"
+		OWNER TO postgres;
