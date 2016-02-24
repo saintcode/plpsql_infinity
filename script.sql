5270300c173a47ed5a2caab7f394d745114cb559
Vadim Evseev <v.evseev@smcs.com.ua>
2016-02-24 16:43:57 +0200

    create function

---
+++ script.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION cs_refresh_mviews() RETURNS char AS $$
+DECLARE
+    mviews RECORD;
+BEGIN
+    RAISE NOTICE 'Refreshing materialized views...';
+
+    FOR mviews IN SELECT * FROM cs_materialized_views ORDER BY sort_key LOOP
+        RAISE NOTICE 'Refreshing materialized view %s ...', quote_ident(mviews.mv_name);
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(mviews.mv_name);
+        EXECUTE 'INSERT INTO '
+                   || quote_ident(mviews.mv_name) || ' '
+                   || mviews.mv_query;
+    END LOOP;
+
+    RAISE NOTICE 'Done refreshing materialized views.';
+    RETURN 1;
+END;
+$$ LANGUAGE plpgsql;
