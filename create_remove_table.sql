e08529ff5a9ae76c73ef74f029bcf59c1a159219
saintcode <sscode@gmail.com>
2016-02-25 23:18:30 +0200

    clear comments

--- create_remove_table.sql
+++ create_remove_table.sql
@@ -2,13 +2,6 @@
 DROP TABLE IF EXISTS public."AA_remove";
 DROP SEQUENCE IF EXISTS public."AA_remove_id_seq";
 
---CREATE SEQUENCE public."AA_remove_id_seq"
---	INCREMENT 1
---	MINVALUE 1
---	MAXVALUE 9223372036854775807
---	START 1
---	CACHE 1;
-
 CREATE TABLE public."AA_remove"
 (
 	id bigserial NOT NULL,
